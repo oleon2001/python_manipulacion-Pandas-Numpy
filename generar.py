38dd87a1f4066e128711447835cb6d6957e23b9c
oleon2001 <oswaldoleon72@gmail.com>
2024-07-01 16:19:06 -0400

    agregando dimensiones

---
+++ generar.py
@@ -0,0 +1,32 @@
+import random
+
+def generar_contrasena(longitud, caracteres):
+  """
+  Genera una contraseña aleatoria con la longitud y caracteres especificados.
+
+  Args:
+    longitud: La longitud deseada para la contraseña (int).
+    caracteres: Cadenas que contienen los grupos de caracteres a incluir (str).
+
+  Returns:
+    Contraseña generada aleatoriamente (str).
+  """
+  contrasena = ""
+  for _ in range(longitud):
+    contrasena += random.choice(caracteres)
+  return contrasena
+
+# Ejemplo de uso
+longitud_contrasena = 12
+caracteres_minusculas = "abcdefghijklmnopqrstuvwxyz"
+caracteres_mayusculas = caracteres_minusculas.upper()
+caracteres_numeros = "0123456789"
+caracteres_simbolos = "!@#$%^&*()_-+={}[]|;:<>,.?"
+
+contrasena_generada = generar_contrasena(longitud_contrasena,
+                                       caracteres_minusculas + 
+                                       caracteres_mayusculas + 
+                                       caracteres_numeros + 
+                                       caracteres_simbolos)
+
+print("Tu contraseña generada es:", contrasena_generada)
